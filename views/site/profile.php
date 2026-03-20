fee339a0da40ac23b65eff64bbfb828e98f05ed9
kawaiierror <sunnata30@gmail.com>
2026-03-20 14:00:51 +0700

    day5 - start making administration panel

--- views/site/profile.php
+++ views/site/profile.php
@@ -6,3 +6,12 @@
 <h3 class="profile_field">Адрес: <?= app()->auth::user()->adress ?></h3>
 <h3 class="profile_field">Телефон: <?= app()->auth::user()->phone ?></h3>
 <h3 class="profile_field">Роль: <?= app()->auth::user()->role ?></h3>
+
+<?php if (app()->auth::check() && app()->auth::user()->role === 'администратор'): ?>
+    <p class="profile_field admin_marker">Вы являетесь администратором</p>
+<!--Здесь будет листинг всех пользователей, а также возможность поменять роль на библиотекаря с дефолт пользователя-->
+<!--Также будет функция удаления пользователя/библиотекаря либо лучше блокировка пользователя-->
+<?php else: ?>
+    <p class="profile_field user_marker">Вы НЕ являетесь администратором</p>
+<?php endif; ?>
+
