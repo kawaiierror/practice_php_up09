22b5f2ca9527ff66ffa065e7aa5515be227baf9b
kawaiierror <sunnata30@gmail.com>
2026-04-23 02:27:12 +0700

    reworking styles

--- views/book/book_list.php
+++ views/book/book_list.php
@@ -3,11 +3,13 @@
 
 <?php //if (app()->auth::user()->role === 'администратор' || app()->auth::user()->role === 'библиотекарь'):?>
 <?php if (app()->auth::user()->role === 'библиотекарь'):?>
+<div class="book_btn_list">
     <a class="book_btn create" href="<?= app()->route->getUrl('/add_book') ?>">Создать книгу</a>
     <br>
     <a class="book_btn create" href="<?= app()->route->getUrl('/add_category') ?>">Создать категорию</a>
     <br>
     <a class="book_btn create" href="<?= app()->route->getUrl('/add_author') ?>">Создать автора</a>
+</div>
 <?php endif; ?>
 
 
@@ -16,32 +18,33 @@
         <div class="book-item">
 
             <?php if ($book->image): ?>
-                <img src="<?= app()->route->getUrl('/public') ?><?=$book->image ?>" alt="<?= $book->book_name ?>" width="250">
+                <img class="book_list_img" src="<?= app()->route->getUrl('/public') ?><?=$book->image ?>" alt="<?= $book->book_name ?>" width="250">
 
             <?php else: ?>
                 <p>нет фото</p>
             <?php endif; ?>
 
-            <h4><?= $book->book_name ?></h4>
-            <p><b>Автор: </b>  <?= $book->author->name ?? '' ?>
-                <?= $book->author->lastname ?? 'Неизвестен' ?></p>
-            <p><b>Категория: </b> <?= $book->category->name ?? 'Без категории' ?></p>
-            <?php if ($book->is_new == 1): ?>
-                <span style="color: green; font-weight: bold; text-transform: uppercase; font-size: 0.8em;">
+            <div class="book-item-info">
+                <h4 class="book_item_title"><?= $book->book_name ?></h4>
+                <p><b>Автор: </b>  <?= $book->author->name ?? '' ?>
+                    <?= $book->author->lastname ?? 'Неизвестен' ?></p>
+                <p><b>Категория: </b> <?= $book->category->name ?? 'Без категории' ?></p>
+                <?php if ($book->is_new == 1): ?>
+                    <span style="color: green; font-weight: bold; text-transform: uppercase; font-size: 0.8em;">
                 Новинка!
             </span>
-            <?php endif; ?>
+                <?php endif; ?>
 
-            <?php if ($book->status !== 'забронирована'): ?>
-                <p>В наличии</p>
-            <?php else: ?>
-                <p style="color: #dc3545"><strong>Уже занята</strong></p>
-            <?php endif; ?>
-            <br>
+                <?php if ($book->status !== 'забронирована'): ?>
+                    <p>В наличии</p>
+                <?php else: ?>
+                    <p style="color: #dc3545"><strong>Уже занята</strong></p>
+                <?php endif; ?>
 
-            <a href="<?= app()->route->getUrl('/book_detail') ?>?id=<?= $book->isbn ?>">
-                Посмотреть подробнее
-            </a>
+                <a class="book_item_link" href="<?= app()->route->getUrl('/book_detail') ?>?id=<?= $book->isbn ?>">
+                    Посмотреть подробнее
+                </a>
+            </div>
         </div>
     <?php endforeach; ?>
 </div>
